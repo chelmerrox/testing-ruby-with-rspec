d68b82b64d0217640f9a26e67bd0b5bc3f61fe7c
Losalini <C.Rokocakau@gmail.com>
2020-10-20 04:03:21 +1200

    Add more test cases forall instance method definitions

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -39,18 +39,33 @@ describe Calculator do
         expect(calculator.add(-1, -2, -3)).to eql(-6)
        end
 
+       it "returns nil for TypeError" do
+        calculator = Calculator.new
+        expect(calculator.add(1, 'a')).to eql(nil)
+       end
+
+       it "returns nil for TypeError" do
+        calculator = Calculator.new
+        expect(calculator.add('a', 'a')).to eql(nil)
+       end
+
+       it "returns nil for TypeError" do
+        calculator = Calculator.new
+        expect(calculator.add(1, 'a', 'b')).to eql(nil)
+       end
+
        # negative scenarios
-      it "returns the sum of two numbers" do
+      it "should not return zero" do
         calculator = Calculator.new
         expect(calculator.add(0, 2)).not_to eql(0)
       end
 
-      it "returns the sum of more than two numbers" do
+      it "should not return three" do
         calculator = Calculator.new
         expect(calculator.add(2, 5)).not_to eql(3)
       end
 
-      it "returns the sum of more than two numbers" do
+      it "should not return -4" do
         calculator = Calculator.new
         expect(calculator.add(1, 2, 3)).not_to eql(-4)
       end
@@ -73,18 +88,33 @@ describe Calculator do
         expect(calculator.subtract(-1,-5, 6)).to eql(-2)
       end
 
+      it "returns nil for TypeError" do
+        calculator = Calculator.new
+        expect(calculator.subtract(1, 'a')).to eql(nil)
+      end
+
+      it "returns nil for TypeError" do
+        calculator = Calculator.new
+        expect(calculator.subtract('a', 'a')).to eql(nil)
+      end
+
+      it "returns nil for TypeError" do
+        calculator = Calculator.new
+        expect(calculator.subtract(1, 'a', 'b')).to eql(nil)
+      end
+
       # negative scenarios
-      it "returns the difference of two number" do
+      it "should not return zero" do
         calculator = Calculator.new
         expect(calculator.subtract(10,2)).not_to eql(0)
       end
 
-      it "returns the difference of two number" do
+      it "should not return -6" do
         calculator = Calculator.new
         expect(calculator.subtract(1,2,3)).not_to eql(-6)
       end
 
-      it "returns the difference of two number" do
+      it "should not return -6" do
         calculator = Calculator.new
         expect(calculator.subtract(-1,-2,-3)).not_to eql(-6)
       end
@@ -105,6 +135,21 @@ describe Calculator do
         calculator = Calculator.new
         expect(calculator.multiply(2,3,-1)).to eql(-6)
       end
+
+      it "returns nil for TypeError" do
+        calculator = Calculator.new
+        expect(calculator.multiply(1, 'a')).to eql(nil)
+      end
+
+      it "returns nil for TypeError" do
+        calculator = Calculator.new
+        expect(calculator.multiply('a', 'a')).to eql(nil)
+      end
+
+      it "returns nil for TypeError" do
+        calculator = Calculator.new
+        expect(calculator.multiply(1, 'a', 'b')).to eql(nil)
+      end
     end
 
     describe "#divide" do
@@ -119,24 +164,24 @@ describe Calculator do
         expect(calculator.divide(6,-3)).to eql(-2)
       end
 
-      it "returns the quotient of two numbers" do
-        calculator = Calculator.new
-        expect(calculator.divide(6,0)).to eql(ZeroDivisionError)
-      end
-
       it "returns the quotient of more than two numbers" do
         calculator = Calculator.new
         expect(calculator.divide(6,3,-2)).to eql(-1)
       end
 
-      it "returns the quotient of more than two numbers" do
+      it "returns nil for ZeroDivisionError" do
         calculator = Calculator.new
-        expect(calculator.divide(6,0,-2)).to eql(ZeroDivisionError)
+        expect(calculator.divide(6,0)).to eql(nil)
       end
 
-      it "returns the quotient of more than two numbers" do
+      it "returns nil for ZeroDivisionError" do
         calculator = Calculator.new
-        expect(calculator.divide(6,3,0)).to eql(ZeroDivisionError)
+        expect(calculator.divide(6,0,-2)).to eql(nil)
+      end
+
+      it "returns nil for ZeroDivisionError" do
+        calculator = Calculator.new
+        expect(calculator.divide(6,3,0)).to eql(nil)
       end
 
       # negative scenarios
